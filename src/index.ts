4422a19d5f8811c830decadc2770f283e335f66c
Himanshu Negi <himanshu.21negi@gmail.com>
2026-01-20 17:07:48 +0530

    Route protection(authnetication) added, user settings update route added

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,25 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import * as dynamoose from "dynamoose";
+import {
+  clerkMiddleware,
+  createClerkClient,
+  requireAuth,
+} from "@clerk/express";
 /* ROUTE IMPORTS */
 import courseRoutes from "./routes/courseRoutes";
+import userClerkRoutes from "./routes/userClerkRoutes";
+
 /* CONFIGURATIONS */
 dotenv.config();
-
 const isProduction = process.env.NODE_ENV === "production";
-
 if (!isProduction) {
   dynamoose.aws.ddb.local();
 }
+
+export const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
 
 const app = express();
 app.use(express.json());
@@ -24,12 +33,14 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(clerkMiddleware());
 
 // ROUTES
 app.get("/", (req, res) => {
   res.send("API is up and running as it needs to be");
 });
 app.use("/courses", courseRoutes);
+app.use("/users/clerk", requireAuth(), userClerkRoutes);
 
 // SERVER
 const port = process.env.PORT || 3000;
