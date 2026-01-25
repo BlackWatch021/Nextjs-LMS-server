a020b04ab9a1f5b583463f8c5fd1033efa277685
Himanshu Negi <himanshu.21negi@gmail.com>
2026-01-25 21:55:08 +0530

    Controllers and routes for coures CRUD uperations

--- src/controllers/courseControllers.ts
+++ src/controllers/courseControllers.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from "express";
 import Course from "../models/courseModel";
+import { v4 as uuidv4 } from "uuid";
+import { title } from "process";
+import { getAuth } from "@clerk/express";
 
 export const listCourses = async (
   req: Request,
-  res: Response
+  res: Response,
 ): Promise<void> => {
   const { category } = req.query;
   try {
@@ -30,5 +33,125 @@ export const getCourse = async (req: Request, res: Response): Promise<void> => {
     res.json({ message: "Course retrieved successfully", data: course });
   } catch (error) {
     res.status(500).json({ message: "Error retrieving course", error });
+  }
+};
+
+export const createCourse = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const { teacherId, teacherName } = req.body;
+    if (!teacherId || !teacherName) {
+      res.status(400).json({ message: "Teacher Id and name are required" });
+    }
+
+    const newCourse = new Course({
+      courseId: uuidv4(),
+      teacherId,
+      teacherName,
+      title: "Untitled Course",
+      description: "",
+      category: "Uncategorized",
+      image: "",
+      level: "Beginner",
+      price: 0,
+      status: "Draft",
+      sections: [],
+      enrollments: [],
+    });
+
+    await newCourse.save();
+
+    res.json({ message: "Course created successfully", data: newCourse });
+  } catch (error) {
+    res.status(500).json({ message: "Error creating course", error });
+  }
+};
+
+export const updateCourse = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  // const { courseId } = req.params;
+  const courseId = req.params.courseId as string;
+  const updateData = { ...req.body };
+  const { userId } = getAuth(req);
+
+  try {
+    const course = await Course.get(courseId);
+    if (!course) {
+      res.status(404).json({ message: "Course not found" });
+      return;
+    }
+
+    if (course.teacherId !== userId) {
+      res
+        .status(403)
+        .json({ message: "Not authorized to update this course " });
+      return;
+    }
+
+    if (updateData.price) {
+      const price = parseInt(updateData.price);
+      if (isNaN(price)) {
+        res.status(400).json({
+          message: "Invalid price format",
+          error: "Price must be a valid number",
+        });
+        return;
+      }
+      updateData.price = price * 100;
+    }
+
+    if (updateData.sections) {
+      const sectionsData =
+        typeof updateData.sections === "string"
+          ? JSON.parse(updateData.sections)
+          : updateData.sections;
+
+      updateData.sections = sectionsData.map((section: any) => ({
+        ...section,
+        sectionId: section.sectionId || uuidv4(),
+        chapters: section.chapters.map((chapter: any) => ({
+          ...chapter,
+          chapterId: chapter.chapterId || uuidv4(),
+        })),
+      }));
+    }
+
+    Object.assign(course, updateData);
+    await course.save();
+
+    res.json({ message: "Course updated successfully", data: course });
+  } catch (error) {
+    res.status(500).json({ message: "Error updating course", error });
+  }
+};
+
+export const deleteCourse = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  const courseId = req.params.courseId as string;
+  const { userId } = getAuth(req);
+
+  try {
+    const course = await Course.get(courseId);
+    if (!course) {
+      res.status(404).json({ message: "Course not found" });
+      return;
+    }
+
+    if (course.teacherId !== userId) {
+      res.status(403).json({ message: "Not authorized to delete this course" });
+      return;
+    }
+
+    await Course.delete(courseId);
+
+    res.json({ message: "Course deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleteing course", error });
   }
 };
